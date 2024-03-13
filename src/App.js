f1fa2f1a524eadd0856a86329e18c831b3255053
vijayharshamade <vharsha736.vh@gmail.com>
2024-03-14 01:18:13 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>(
+ <>
+  <div className="super-over-league-container">
+    <h1 className='heading'>Super Over League</h1>
+    <div className="teams-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="logo" className='logo-img' />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="logo"  className='logo-img' />
     </div>
-  );
-}
-
-export default App;
+  </div>
+ </> 
+)
+export default App
